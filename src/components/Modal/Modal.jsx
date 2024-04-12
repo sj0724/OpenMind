221fae4a88ae4a4117cb7e205594dae4b6680287
yup299 <yup299@gmail.com>
2024-04-12 12:30:51 +0900

    feat : Modal css ui추가
    Modal의 웹 크기의 css를 추가하였다.

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,11 +1,31 @@
+import * as S from "./Modal.styled";
+import messagesicon from "../../assets/messages-black.svg";
+import xMark from "../../assets/x-mark.svg";
+
 function Modal() {
   return (
-    <div className="modal">
-      <div className="modal-content">
-        <span className="close">&times;</span>
-        <p>Some text in the Modal..</p>
-      </div>
-    </div>
+    <S.StyledModal>
+      <S.ModalWrapper>
+        <S.Header>
+          <div>
+            <img src={messagesicon} alt="Modal창 header 좌측 메시지모양 아이콘" />
+            <span>질문을 작성하세요</span>
+          </div>
+          <img src={xMark} alt="Modal창 header 우측 X마크모양 아이콘" />
+        </S.Header>
+        <S.ToUser>
+          <span>To.</span>
+          <S.Profile />
+          <p>username</p>
+        </S.ToUser>
+        <S.Content>
+          <textarea placeholder="내용을 입력해주세요"></textarea>
+        </S.Content>
+        <S.Footer>
+          <span>질문 보내기</span>
+        </S.Footer>
+      </S.ModalWrapper>
+    </S.StyledModal>
   );
 }
 
---
+++ src/components/Modal/Modal.styled.js
@@ -0,0 +1,104 @@
+import styled from "styled-components";
+
+export const StyledModal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.56); //불투명토조정
+  z-index: 5;
+`;
+
+export const ModalWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: var(--Grayscale-10);
+  border-radius: 24px;
+  width: 61.2rem;
+  height: 45.4rem;
+  justify-content: center;
+  align-items: center;
+  margin-top: 3rem;
+  gap: 0.6rem;
+`;
+
+export const Header = styled.div`
+  width: 53.2rem;
+  height: 3rem;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 3rem;
+
+  & div {
+    display: flex;
+    align-items: center;
+    gap: 0.8rem;
+  }
+
+  & span {
+    font-size: 2.4rem;
+    color: var(--Grayscale-90);
+  }
+`;
+
+export const ToUser = styled.div`
+  display: flex;
+  align-items: center;
+  width: 53.2rem;
+  gap: 0.4rem;
+  margin-bottom: 1rem;
+
+  & span {
+    font-size: 1.8rem;
+    color: var(--Grayscale-60);
+  }
+
+  & p {
+    font-size: 1.6rem;
+  }
+`;
+
+export const Profile = styled.div`
+  width: 28px;
+  height: 28px;
+  background-color: gray;
+  border-radius: 9999px;
+`;
+
+export const Content = styled.div`
+  width: 53.2rem;
+  height: 18rem;
+  border-radius: 0.8rem;
+  padding: 1.6rem;
+  gap: 1rem;
+  background-color: var(--Grayscale-20);
+
+  & textarea {
+    width: 50rem;
+    height: 14.8rem;
+    font-size: 1.6rem;
+    background-color: var(--Grayscale-20);
+    color: var(--Grayscale-40);
+    border: none;
+    outline: none;
+    resize: none;
+  }
+`;
+
+export const Footer = styled.button`
+  display: flex;
+  width: 53.2rem;
+  height: 4.6rem;
+  border-radius: 0.8rem;
+  padding: 1.2rem, 2.4rem;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  background-color: var(--Brown-30);
+  font-size: 1.6rem;
+  color: var(--Grayscale-10);
+`;
