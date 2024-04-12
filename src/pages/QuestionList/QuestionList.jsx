e618a4cb48c9fe6b31b62c72cbbd7d1d463f8f43
yup299 <yup299@gmail.com>
2024-04-12 09:04:33 +0900

    feat : 모달 컴포넌트 추가
    QuestionList 컴포넌트안에 모달 컴포넌트를 추가하여 질문을 클릭하면 모달이 뜨도록 구현하였습니다.
    단 모달의 내용은 아직 구현하지 않았습니다.

--- src/pages/QuestionList/QuestionList.jsx
+++ src/pages/QuestionList/QuestionList.jsx
@@ -6,22 +6,27 @@ import * as S from "./QuestionList.styled";
 import emptyIcon from "../../assets/emptyIcon.svg";
 import mainLogo from "../../assets/logo.png";
 import UserProfile from "../../components/UserProfile/UserProfile";
+import Modal from "../../components/Modal/Modal";
 
 function QuestionList() {
   const [question, setQuestion] = useState(true);
+  const [modal, setModal] = useState(false);
+
+  const handleModalToggle = () => {
+    setModal(!modal);
+  };
 
   return (
     <>
+      {modal && <Modal />}
       <S.Header>
         <img src={mainLogo} alt="mainLogo" />
       </S.Header>
       <UserProfile />
       <S.Body>
-        <QuestionContainer>
-          {question ? <Question /> : <S.No_Question src={emptyIcon} />}
-        </QuestionContainer>
+        <QuestionContainer>{question ? <Question /> : <S.No_Question src={emptyIcon} />}</QuestionContainer>
       </S.Body>
-      <S.FloatingBtn>질문 작성하기</S.FloatingBtn>
+      <S.FloatingBtn onClick={handleModalToggle}>질문 작성하기</S.FloatingBtn>
     </>
   );
 }
