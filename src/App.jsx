0f676d81cb0733a9011667ce507227f1ef537325
박상준 <parksangjun@bagsangjun-ui-MacBookAir.local>
2024-04-12 18:44:52 +0900

    feat: 메인페이지 구성

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import QuestionList from "./pages/QuestionList/QuestionList";
 import AnswerList from "./pages/AnswerList/AnswerList";
 import "./styles/common.css";
-import Home from "./pages/Home";
+import Main from "./pages/Main/Main";
 import List from "./pages/List";
 
 //1. "/" : 메인페이지
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Main />} />
         <Route path="/list" element={<List />} />
         <Route path="/answer" element={<AnswerList />} />
         <Route path="/question" element={<QuestionList />} />
---
+++ src/components/LinkButton/LinkButton.jsx
@@ -0,0 +1,13 @@
+import * as S from "./LinkButton.styled";
+import arrowIcon from "../../assets/arrow-right.svg";
+
+function LinkButton({ children }) {
+  return (
+    <S.LinkBtn>
+      {children}
+      <img src={arrowIcon} alt="arrowIcon" />
+    </S.LinkBtn>
+  );
+}
+
+export default LinkButton;
