2e4031202d5cdd2aeff311e0f566066ae943da52
yup299 <yup299@gmail.com>
2024-04-14 00:36:47 +0900

    fix : 아래의 항목들의 린터적용
    자세한 변경사항은 주석처리 해두었습니다.

--- src/hooks/useFetchCardList.jsx
+++ src/hooks/useFetchCardList.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react';
-import { fetchCardList } from '../services/fetchCardList';
+import fetchCardList from '../services/fetchCardList';
+/* 함수가 단일이라면 export default로 내보내고, 함수가 여러개라면 export로 내보낸다는 규칙때문에 fetchCardList를 변경하고
+   수정함에 있어 import {fetchCardList로} 에서 fetchCardList로 변경하였습니다. */
 
-export const useFetchCardList = () => {
+function useFetchCardList() {
   const [cards, setCards] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -21,4 +23,6 @@ export const useFetchCardList = () => {
   }, []);
 
   return { cards, loading };
-};
+}
+
+export default useFetchCardList;
--- src/services/fetchCardList.jsx
+++ src/services/fetchCardList.jsx
@@ -2,7 +2,9 @@ import axiosinstance from '../utils/axios';
 
 const fetchCardList = async (limit = 8, offset = 0, sort = 'time') => {
   try {
-    const response = await axiosinstance.get(`/subjects/?limit=${limit}&offset=${offset}&sort=${sort}`);
+    const response = await axiosinstance.get(
+      `/subjects/?limit=${limit}&offset=${offset}&sort=${sort}`,
+    );
     return response.data;
   } catch (error) {
     // eslint-disable-next-line no-console
