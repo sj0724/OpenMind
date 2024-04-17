2bd5d68e4401da84d8552e5224536bb7ddc58764
hyunseo11 <70734028+hyunseo11@users.noreply.github.com>
2024-04-17 13:58:03 +0900

    fix: 새로고침 시 페이지 초기화 해결

--- src/hooks/useFetchCardList.js
+++ src/hooks/useFetchCardList.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import fetchCardList from '../services/fetchCardList';
 /* 함수가 단일이라면 export default로 내보내고, 함수가 여러개라면 export로 내보낸다는 규칙때문에 fetchCardList를 변경하고
    수정함에 있어 import {fetchCardList로} 에서 fetchCardList로 변경하였습니다. */
@@ -6,16 +7,24 @@ import fetchCardList from '../services/fetchCardList';
 export const useFetchCardList = (limit, offset, sort) => {
   const [cards, setCards] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [currentPage, setCurrentPage] = useState(1);
+  const location = useLocation();
+
+  // 페이지가 처음 로드될 때 현재 페이지를 로컬 스토리지에서 가져옴
+  const currentPageFromStorage = parseInt(localStorage.getItem('currentPage'), 10) || 1;
+  const [currentPage, setCurrentPage] = useState(currentPageFromStorage);
 
   useEffect(() => {
     const fetchCardListData = async () => {
       try {
-        const calculatedOffset = (currentPage - 1) * limit;
+        const searchParams = new URLSearchParams(location.search);
+        const page = parseInt(searchParams.get('page'), 10) || 1;
+        const calculatedOffset = (page - 1) * limit;
 
         const response = await fetchCardList(limit, calculatedOffset, sort);
         setCards(response.results);
         setLoading(false);
+        setCurrentPage(page); // 페이지 업데이트
+        localStorage.setItem('currentPage', page); // 로컬 스토리지 업데이트
       } catch (error) {
         // console.log('Error fetching card list:', error);
         setLoading(false);
@@ -23,7 +32,7 @@ export const useFetchCardList = (limit, offset, sort) => {
     };
 
     fetchCardListData();
-  }, [limit, sort, currentPage, setCurrentPage]);
+  }, [limit, sort, location.search]);
 
   return {
     cards,
