0d92cf590f8986b2ee35493c120cb1d1efc0405e
yup299 <yup299@gmail.com>
2024-04-23 15:07:32 +0900

    feat : modal창 글자수 제한 및 토스트알리미 추가

---
+++ src/components/SendToast/SendToast.jsx
@@ -0,0 +1,41 @@
+// SendToast 컴포넌트
+import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+import ToastBody from './SendToast.styled';
+
+function SendToast({ setToast, text, index }) {
+  const [fade, setFade] = useState('');
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setToast();
+    }, 5000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [setToast]);
+
+  useEffect(() => {
+    const a = setTimeout(() => {
+      setFade('fade');
+    }, 100);
+
+    return () => {
+      clearTimeout(a);
+    };
+  }, []);
+
+  return (
+    <ToastBody className={fade} top={`${(index + 1) * 6 - 4}rem`}>
+      <p>{text}</p>
+    </ToastBody>
+  );
+}
+
+SendToast.propTypes = {
+  setToast: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+};
+
+export default SendToast;
---
+++ src/components/SendToast/SendToast.styled.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+const ToastBody = styled.div`
+  position: fixed;
+  right: 2rem;
+  top: ${(props) => (props.top !== undefined ? props.top : '2rem')};
+  width: 200px;
+  height: 50px;
+  padding: 1.2rem 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 0.8rem;
+  background: var(--Grayscale-60);
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  opacity: 0;
+
+  p {
+    color: var(--Grayscale-10);
+    font-family: Pretendard;
+    font-size: 1.4rem;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 18px;
+  }
+
+  &.fade {
+    opacity: 1;
+    transition: opacity 0.5s;
+  }
+`;
+
+export default ToastBody;
